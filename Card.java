6961b9d8cf621734fff7f41c4b95966f560f8480
CABABO2000 <65503613+CABABO2000@users.noreply.github.com>
2026-03-11 22:45:18 -0700

    Added Getter and Setter methods

--- Card.java
+++ Card.java
@@ -12,13 +12,25 @@ public class Card{
 	
 	private String url = new String("");
 	private String[] identity = new String[2];
-	private int[] stats = new int[3];
+	private String[] stats = new String[3];
+	private String text = new String("");
+	private String author = new String("");
+	private String set = new String("");
+	private int count = 0;
 	
 	public Card(){
-		url = "https://drive.google.com/uc?export=download&id=1HMHhPj290ICULgHcH0mLeWA_t8DeEYvT";
+		url = "https://drive.google.com/uc?export=download&id=1HMHhPj290ICULgHcH0mLeWA_t8DeEYvT"; // defaults to the standard card background.
 		String[] identity = {"NAME", "SPECIES"};
-		int[] stats = {0, 0, 0};
+		String[] stats = {"0", "0", "0"};
 	}
+	
+	public Card(String[] indentity, String[] stats, String text, String author, String set, int count){
+		
+	}
+	
+	/*
+	*	The Getters
+	*/
 	
 	public BufferedImage getCardBackgroundImage(){
 		
@@ -57,5 +69,123 @@ public class Card{
 		
 		return identitycopy;
 	}
+	
+	public String[] getStats(){
+		
+		String[] statscopy = new String[3];
+		
+		if(this.stats == null){
+			for(int i = 0; i < 3; i++){
+				statscopy[i] = "0";
+			}
+		}
+		if(this.stats[0] == null){
+			statscopy[0] = "0";
+		}
+		if(this.stats[1] == null){
+			statscopy[1] = "0";
+		}
+		if(Integer.parseInt(this.stats[2]) < 0){
+			statscopy[2] = "0";
+		}
+		
+		statscopy = Arrays.copyOf(stats, stats.length);
+		
+		return statscopy;
+	}
+	
+	public String getAuthor(){
+		
+		String authorcopy = new String(author);
+		
+		return authorcopy.toLowerCase();
+	}
+	
+	public String getSet(){
+		
+		String setcopy = new String(set);
+		
+		return setcopy.toLowerCase();
+	}
+	
+	public int getCount(){
+		return this.count;
+	}
+	
+	/*
+	 *	 The Setters
+	 */
+	 
+	 public String setURL(String url){
+		 
+		try{
+			URL urltest = new URL(url);
+		} catch(MalformedURLException mue) {
+			IO.print("Please provide a valid URL");
+			return "";
+		}
+		
+		 return url;
+	 }
+	 
+	 public String[] setIdentity(String[] identity){
+		 
+		if(identity == null){
+			for(int i = 0; i < identity.length; i++){
+				identity[i] = "";
+			}
+		}
+		if(identity[0] == null){
+			identity[0] = "";
+		}
+		if(identity[1] == null){
+			identity[1] = "";
+		}
+		 
+		 return identity;
+	 }
+	 
+	 public String[] setStats(String[] stats){
+		 
+		if(stats == null){
+			for(int i = 0; i < stats.length; i++){
+				stats[i] = "0";
+			}
+		}
+		if(this.stats[0] == null){
+			stats[0] = "0";
+		}
+		if(this.stats[1] == null){
+			stats[1] = "0";
+		}
+		if(Integer.parseInt(this.stats[2]) < 0){
+			stats[2] = "0";
+		}
+		 return stats;
+	 }
+	 
+	 public String setAuthor(String author){
+		 if(author == null){
+			 author = "John Anonymous";
+		 }
+		 
+		 return author;
+	 }
+	 
+	 public String setSet(String set){
+		 if(set == null){
+			 set = "IRL";
+		 }
+		 
+		 return set;
+	 }
+	 
+	 public int setCount(int count){
+		 if(count < 0){
+			 count = 0;
+		 }
+		 
+		 return count;
+	 }
 	
 }
